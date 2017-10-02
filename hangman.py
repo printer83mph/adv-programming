59403f9a2242125952ca496b0e171676a98b8a4b
Thomas Shaw <printer.83mph@gmail.com>
2017-10-02 13:16:14 -0400

    Add Will's de-lineified hangman

--- hangman.py
+++ hangman.py
@@ -1,11 +1,16 @@
 from random import choice
-
+'''
 def play(wordlist,tries):
     word = choice(wordlist)
     triesleft = tries
     guessedletters = ""
     gamefinished = False
     while gamefinished == False:
+        output = word
+        for i in range(len(output)):
+            if output[i] not in guessedletters:
+                output = "%s%s%s"%(output[:i],"_",(output[i+1:]))
+        print output
         print triesleft, " tries left."
         letterguess = raw_input("What letter? ")[0].lower()
         if letterguess not in word:
@@ -16,17 +21,34 @@ def play(wordlist,tries):
                 triesleft -= 1
         else: print "Yup!"
         guessedletters += letterguess
-        output = word
-        for i in range(len(output)):
-            if output[i] not in guessedletters:
-                output = "%s%s%s"%(output[:i],"_",(output[i+1:]))
-        print output
         if triesleft == 0:
             print "Game over! The word was ", word
             gamefinished = True
         elif set(word).issubset(set(guessedletters)):
-            print "You win!"
+            print "You win! the word was ", word
             gamefinished = True
+
+if __name__ == "__main__":
+    wl = open("words.txt","r")
+    words = wl.read().split("\n")
+    play(words,6)
+
+'''
+def play(wordlist,tries):
+    word,triesleft,guessedletters,gamefinished = choice(wordlist),tries,"",False
+    while gamefinished == False:
+        output = word
+        for i in range(len(output)):
+            if output[i] not in guessedletters: output = "%s%s%s"%(output[:i],"_",(output[i+1:]))
+        print output,"\n",triesleft, " tries left."
+        letterguess = raw_input("What letter? ")[0].lower()
+        if letterguess not in word: 
+            if letterguess in guessedletters: print "Already used!"
+            else: print "Nope"; triesleft -= 1
+        else: print "Yup!"
+        guessedletters += letterguess
+        if triesleft == 0: print "Game over! The word was ", word; gamefinished = True
+        elif set(word).issubset(set(guessedletters)): print "You win! the word was ", word; gamefinished = True
 
 if __name__ == "__main__":
     wl = open("words.txt","r")
