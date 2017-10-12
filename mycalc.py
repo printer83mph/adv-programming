e504919f29e39d085d51894bb2a200c0e4b33a84
Thomas Shaw <printer.83mph@gmail.com>
2017-10-12 08:16:35 -0400

    Add mycalc

---
+++ mycalc.py
@@ -0,0 +1,22 @@
+def add(a,b):
+	return a + b
+def sub(a,b):
+	return a - b
+def mul(a,b):
+	return a * b
+def div(a,b):
+	return a / b
+
+def fib(a):
+	if a == 0: return 0
+	if a == 1: return 1
+	return(fib(a-1)+fib(a-2))
+
+def ffib(a):
+	num1, num2 = 0,1
+	oldfinal = 1
+	final = 1
+	for i in range(a):
+		final,num1,num2 = num2+final,num2,oldfinal
+		oldfinal = final
+	return num1
