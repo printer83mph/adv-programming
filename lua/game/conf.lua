48a6c62809d9ebaed48dd36f1bfe5f91e526ec5b
Thomas Shaw <printer.83mph@gmail.com>
2017-10-17 11:21:44 -0400

    Add love game

---
+++ lua/game/conf.lua
@@ -0,0 +1,10 @@
+-- Config
+function love.conf(t)
+	t.title = "Supa Combat Arena"
+	t.version = "0.10.2"
+	t.window.width = 800
+	t.window.height = 600
+
+	-- Debugging - rm when done
+	t.console = true
+end
