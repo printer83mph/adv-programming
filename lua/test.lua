2152db0c01ecdb06f0c6c713a17e9ad7c5068443
Thomas Shaw <printer.83mph@gmail.com>
2017-10-17 09:26:08 -0400

    Get rid of like 1 line

--- lua/test.lua
+++ lua/test.lua
@@ -6,21 +6,20 @@ function calc()
 		if tonumber(input) ~= nil then
 			table.insert(inlist,tonumber(input))
 		elseif input == "add" then
-			local out = 0
+			out = 0
 			for key,val in pairs(inlist) do
 				out = out + val
 			end
-			print(out)
 			break
 		elseif input == "mul" then
-			local out = 1
+			out = 1
 			for key,val in pairs(inlist) do
 				out = out * val
 			end
-			print(out)
 			break
 		end
 	end
+	print(out)
 end
 
 calc()
